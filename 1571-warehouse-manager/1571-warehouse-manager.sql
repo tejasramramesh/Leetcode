ef2c20c96faabf6e0ee055a8c3689db4888a9474
tejasramramesh <45738176+tejasramramesh@users.noreply.github.com>
2022-06-27 02:34:20 -0400

    Time: 485 ms (91.91%), Space: 0B (100.00%) - LeetHub

--- 1571-warehouse-manager/1571-warehouse-manager.sql
+++ 1571-warehouse-manager/1571-warehouse-manager.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT DISTINCT W.name as warehouse_name
 , (SUM(W.units * P.width * P.length * P.Height )) as volume
-FROM Warehouse W  JOIN Products P
+FROM Warehouse W  INNER JOIN Products P
     ON ( W.product_id = P.product_id)
 GROUP BY W.name
